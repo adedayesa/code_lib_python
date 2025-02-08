b546e937018274f2514bd0657cd8cf79dc08336a
adedayesa <168404202+adedayesa@users.noreply.github.com>
2025-02-08 18:50:10 +0700

    Penjumlahan.py

--- Penjumlahan.py
+++ Penjumlahan.py
@@ -1,3 +1,8 @@
+# Nama Program : Program Menghitung Penjumlahan
+# Nama Programmer : Adedayesa Nurvitian
+# Waktu Pengerjaan : 29 Januari 2025
+
+# input nilai
 nilai1 = int(input("Masukkan nilai pertama : "))
 nilai2 = int(input("Masukkan nilai kedua : "))
 
