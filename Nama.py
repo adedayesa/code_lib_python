d77f88942b426457fd0cc51de65f3f4a157cfb7a
adedayesa <168404202+adedayesa@users.noreply.github.com>
2025-02-08 18:55:24 +0700

    Nama.py

--- Nama.py
+++ Nama.py
@@ -1,8 +1,15 @@
+# Program : Menghitung panjang string nama
+# Programmer : Adedayesa Nurvitian
+# Tanggal (v1.0) : 29 Januari 2025
+
+# Menampilkan kalimat
 print ("Hari ini","adalah","hari Rabu",sep="*",end="+")
 print ("Gong Xie Fa Chai...")
 
+# Menginput nama
 nama = input("Masukan nama anda: ")
 print ("Nama anda adalah",nama)
 
+# Menghitung panjang string nama
 panjang = len(nama) 
 print("panjang string nama anda adalah ",panjang)
